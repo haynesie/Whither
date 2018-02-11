224a6e011f1dc315f32caec0cb72a34ea00c7b20
haynesie <kevenhaynes@gmail.com>
2018-02-11 00:06:27 -0500

    initial commit

---
+++ createCloginCSV.sh
@@ -0,0 +1,2 @@
+
+for i in lab??; do echo -n "$i: "; grep ':[01] ' ${i} | cut -f1 -d"-" | tr -s ' ' | cut -f1,6,7 -d" " --output-delimiter="," | sed "s/:/,${i},/" | cut -f1-4 -d","; done >> ../../allconsolelogins.csv
